0aa9f2947b70ce58289e54ae579077e7429a6919
Kaoru HAYAMA <iyahaya@nifty.com>
2018-07-05 20:44:14 +0900

    Prototype !

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
+	"github.com/zetamatta/go-mbcs"
 	"github.com/zetamatta/pipe2excel/excel"
 )
 
@@ -34,7 +38,25 @@ func ConvRC(rc string) (int, int) {
 	return row, col
 }
 
+var rcPattern = regexp.MustCompile(`^[A-Za-z]+[0-9]+$`)
+
+var xlsPattern = regexp.MustCompile(`.*\.xls[xm]?$`)
+
 func main1(args []string) error {
+	top := 1
+	left := 1
+	bottom := 200
+	right := 200
+
+	if len(args) >= 1 && rcPattern.MatchString(args[0]) {
+		top, left = ConvRC(args[0])
+		args = args[1:]
+		if len(args) >= 1 && rcPattern.MatchString(args[0]) {
+			bottom, right = ConvRC(args[0])
+			args = args[1:]
+		}
+	}
+
 	excel1, err := excel.New(true)
 	if err != nil {
 		return err
@@ -42,14 +64,15 @@ func main1(args []string) error {
 	defer excel1.Release()
 
 	var book1 *excel.Book
-	if len(args) <= 0 {
-		book1, err = excel1.NewBook()
-	} else {
+	if len(args) > 0 && xlsPattern.MatchString(args[0]) {
 		fname, err := filepath.Abs(args[0])
 		if err != nil {
 			return err
 		}
 		book1, err = excel1.Open(fname)
+		args = args[1:]
+	} else {
+		book1, err = excel1.NewBook()
 	}
 	if err != nil {
 		return err
@@ -60,13 +83,40 @@ func main1(args []string) error {
 	if err != nil {
 		return err
 	}
-	_cell, err := sheet.GetProperty("Cells", 1, 1)
-	if err != nil {
-		return err
+
+	row := top
+	for _, fname := range args {
+		fd, err := os.Open(fname)
+		if err != nil {
+			return err
+		}
+		defer fd.Close()
+		reader := mbcs.Reader(fd, func(error, io.Writer) bool { return false })
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() {
+			col := left
+			line := scanner.Text()
+			for _, c := range line {
+				_cell, err := sheet.GetProperty("Cells", row, col)
+				if err != nil {
+					return err
+				}
+				cell := _cell.ToIDispatch()
+				cell.PutProperty("NumberFormatLocal", "@")
+				cell.PutProperty("Value", string(c))
+				cell.Release()
+				col++
+				if col > right {
+					col = left
+					row++
+				}
+			}
+			row++
+			if row > bottom {
+				return nil
+			}
+		}
 	}
-	cell := _cell.ToIDispatch()
-	cell.PutProperty("Value", "!!!!")
-	cell.Release()
 	return nil
 }
 
